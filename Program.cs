ffcc102ca9ab4df28a8a01f09003a9a762afa0c8
nazmiddin <nazmiddinsirojiddinov080@gmail.com>
2024-08-01 23:33:33 +0500

    1. Doira yuzi va aylana uzunligini hisoblash

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿//1. Doira yuzi va aylana uzunligini hisoblash
+Console.Write("Radiusni kiriting : ");
+string radiusStr = Console.ReadLine();
+int radius = Convert.ToInt32(radiusStr);
+double S = Math.PI * radius * radius;
+double L = 2 * Math.PI * radius;
+Console.WriteLine($" Doiraning yuzi : {S}");
+Console.WriteLine($" Aylana uzunligi : {L}");
